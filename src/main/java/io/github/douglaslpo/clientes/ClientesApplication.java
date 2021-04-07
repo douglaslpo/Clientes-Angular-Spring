70cff66087445f344650e89349a09cf6aaad61e7
DouglasLPO <douglaslpo@bol.com.br>
2021-04-07 19:11:59 -0300

    Initial commit, fist project Angular e Spring boot. Primeiro projeto Angular Front e Spring Boot Back

---
+++ src/main/java/io/github/douglaslpo/clientes/ClientesApplication.java
@@ -0,0 +1,14 @@
+package io.github.douglaslpo.clientes;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ClientesApplication {
+
+    public static void main(String[] args) {
+        run(ClientesApplication.class, args);
+
+    }
+}
